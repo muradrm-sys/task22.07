0e87b09d0292b7e669dead998e9ae4644a7d4cb3
muradrm-sys <muradrm@code.edu.az>
2026-07-22 23:09:04 +0400

    first commit

---
+++ program.cs
@@ -0,0 +1,40 @@
+//Verilmiş ədədi kvadratına yüksəldən metod. (Metodu cağırdıqdan sonra variable kvadratına yüksəlmiş olmalıdı)
+#region 1
+//static int Power(int kvadrat)
+//{
+//    kvadrat = kvadrat * kvadrat;
+//    return kvadrat;
+//        }
+//Console.WriteLine(Power(5));
+#endregion
+#region 2
+//Verilmiş ədədlər siyahısını içindəki bütün
+// ədədlər kvadratına yüksəlmiş array-ə çevirən metod
+#endregion
+#region 3
+//Parametr olaraq 1 string dəyər qəbul edən və həmin string dəyəri icində bosluqlar qalmayacaq hala gətirən metod.
+//Misal olaraq,
+//name = " Murad Mammadli " deyə bir variable-mız varsa onu yaratdigimiz
+//metoda göndərdikdə variable-daki dəyər "HikmətAbbasov" olmalıdır.
+//string Nospace(string namesurname)
+//{
+//    string bos = "";
+//    for (int i = 0; i < namesurname.Length; i++)
+//    {
+//        if (namesurname[i] != ' ')
+//        {
+//            bos += namesurname[i];
+//        }
+//    }
+//    return bos;
+
+//}
+//Console.WriteLine(Nospace("Pepe Erdogan"));
+#endregion
+#region 4
+//4.Parameter olaraq integer array variable-i
+//ve bir integer value qebul eden ve hemin integer
+//value-nu integer array-e yeni element kimi elave eden metod. Misal üçün int[] nums = {1,5,7} deyə bir variable-mız var.
+//yazdığımız metodu çağırıb arqument olaraq nums və 3 göndərsək nums arrayının dəyəri  {1,5,7,3} olmalıdır.
+
+#endregion
